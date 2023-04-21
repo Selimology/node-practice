46e94cb5d19a590926b8286fd839247855de5736
Selimology <kertekin@entos.dev>
2023-04-21 04:27:39 +0200

    feat: add basic validation

--- src/router.ts
+++ src/router.ts
@@ -1,4 +1,10 @@
 import { Router } from "express";
+import {
+  body,
+  oneOf,
+  validationResult,
+} from "express-validator";
+import { handleInputErrors } from "./modules/middleware";
 
 const router = Router();
 
@@ -8,26 +14,71 @@ router.get("/product", (req, res) => {
   res.json({ message: "hello" });
 });
 router.get("/product/:id", () => {});
-router.put("/product/:id", () => {});
-router.post("/product/", (req,res) => {
-  res.json({ message: "hello" });
-});
+router.put(
+  "/product/:id",
+  [body("name").isString(), handleInputErrors],
+  (req, res) => {}
+);
+router.post(
+  "/product",
+  [body("name").isString(), handleInputErrors],
+  (req, res) => {}
+);
 router.delete("/product/:id", () => {});
 
 // Update
 
 router.get("/update", () => {});
 router.get("/update/:id", () => {});
-router.put("/update/:id", () => {});
-router.post("/update/", () => {});
+router.put(
+  "/update/:id",
+  [
+    body("title").optional(),
+    body("body").optional(),
+    oneOf([
+      body("status").equals("IN_PROGRESS"),
+      body("status").equals("SHIPPED"),
+      body("status").equals("DEPRACATED"),
+    ]),
+    body("version").optional(),
+  ],
+  () => {}
+);
+router.post(
+  "/update/",
+  body("title").exists(),
+  body("body").exists().isString(),
+  oneOf([
+    body("status").equals("IN_PROGRESS"),
+    body("status").equals("SHIPPED"),
+    body("status").equals("DEPRACATED"),
+  ]),
+  body("version").optional(),
+  () => {}
+);
 router.delete("/update/:id", () => {});
 
 // Update Points
 
 router.get("/updatepoint", () => {});
 router.get("/updatepoint/:id", () => {});
-router.put("/updatepoint/:id", () => {});
-router.post("/updatepoint/", () => {});
+router.put(
+  "/updatepoint/:id",
+  [
+    body("name").optional().isString(),
+    body("description").optional().isString(),
+  ],
+  () => {}
+);
+router.post(
+  "/updatepoint/",
+  [
+    body("name").exists().isString(),
+    body("description").exists().isString(),
+    body('updateId').exists().isString()
+  ],
+  () => {}
+);
 router.delete("/updatepoint/:id", () => {});
 
 export default router;
